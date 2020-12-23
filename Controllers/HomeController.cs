e245962d4158ba1a489cd2ec738f6c00d64490ca
dimkob8139 <dimkob8139@gmail.com>
2020-12-23 12:38:44 +0200

    second commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace TheWayShop.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult About()
         {
             return View();
         }
